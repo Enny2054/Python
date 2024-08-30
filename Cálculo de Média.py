2948b4f7f56edc433c05eb3fad9c476afe3c4d2f
Adrienny Thiffanny <165061856+Enny2054@users.noreply.github.com>
2024-08-30 19:24:49 -0300

    Add files via upload

---
+++ Cálculo de Média.py
@@ -0,0 +1,8 @@
+#Crie um programa para calcular a média de duas notas.
+
+N1 = float(input("Digite a primeira nota: "))
+N2 = float(input("Digite a segunda nota: "))
+
+media = ( N1 + N2 ) / 2
+
+print("A média das notas é: ", media)
