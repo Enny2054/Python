8d0fdffe4f10143b49aba91579e24f6bbcdd2303
Adrienny Thiffanny <165061856+Enny2054@users.noreply.github.com>
2024-09-13 19:30:01 -0300

    Aumento Salarial.py

--- Aumento Salarial.py
+++ Aumento Salarial.py
@@ -1,4 +1,5 @@
-# Uma empresa concederá um aumento de salário aos seus funcionários, variável de acordo com o cargo, conforme a tabela abaixo.
+# Uma empresa concederá um aumento de salário aos seus funcionários, variável de acordo 
+# com o cargo, conforme a tabela abaixo.
 # Faça um algoritmo que leia o salário e o cargo de um funcionário e calcule o novo salário. 
 # Se o cargo do funcionário não estiver na tabela, ele deverá, então, receber 40% de aumento. 
 # Mostre o salário antigo, o novo salário e a diferença.
